7f70f45bde575a3ea4e1e46bbdb58722713b791a
Mark Luo <macluo@gmail.com>
2015-04-07 13:49:47 -0400

    Improved termination request

--- app/controllers/termination_requests_controller.rb
+++ app/controllers/termination_requests_controller.rb
@@ -11,10 +11,31 @@ class TerminationRequestsController < ApplicationController
   # GET /termination_requests/1
   # GET /termination_requests/1.json
   def show
+    if (is_student?)
+      redirect_to menu_student_url if !has_pending_request?
+      @person = Person.find_by_pid(session[:pid])
+      @student = Student.find_by_sid(session[:pid])
+      request = TerminationRequest.where("sid = :student_id AND app_status <= 1", {student_id: current_user_id}).first
+      if !request
+        flash.now.alert = 'No pending request is found'
+        #redirect_to menu_student_url
+      end
+    elsif (is_adm?)
+      request = TerminationRequest.find_by_req_no(params[:id])
+      @person = Person.find_by_pid(request.sid)
+      @student = Student.find_by_sid(request.sid)
+      @lease_no = get_active_lease(request.sid).lease_no
+      if !request
+        flash.now.alert = 'No pending request is found'
+        #redirect_to menu_student_url
+      end
+    end
+    @termination_request = request
   end
 
   # GET /termination_requests/new
   def new
+    redirect_to menu_student_url if has_pending_request? || !has_active_lease?
     @person = Person.find_by_pid(session[:pid])
     @student= Student.find_by_sid(session[:pid])
     lease = SignedLease.getCurrentLease(session[:pid])
@@ -78,8 +99,17 @@ class TerminationRequestsController < ApplicationController
       @termination_request = TerminationRequest.find(params[:id])
     end
 
+    def has_pending_request?
+      request = TerminationRequest.where("sid = :student_id AND app_status <= 1", {student_id: current_user_id}) # pending or reviewing
+      if request.count > 0
+        return true
+      else
+        return false
+      end
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def termination_request_params
-      params[:termination_request]
+      params[:termination_request].permit! #allow all parameters for now
     end
 end
